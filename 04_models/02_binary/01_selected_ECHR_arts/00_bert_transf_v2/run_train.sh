553af8ff3e8e849cfa556835dc23c6197e1ff40e
Antonio Santiago Ibanez Lopez <sibanez@rosetta16.csail.mit.edu>
2021-06-23 19:50:34 -0400

    update

--- 04_models/02_binary/01_selected_ECHR_arts/00_bert_transf_v2/run_train.sh
+++ 04_models/02_binary/01_selected_ECHR_arts/00_bert_transf_v2/run_train.sh
@@ -1,14 +1,14 @@
 #INPUT_DIR=C:/Users/siban/Dropbox/CSAIL/Projects/12_Legal_Outcome_Predictor/00_data/v2/01_preprocessed/01_50pars_256_tok/02_full_binary
 #OUTPUT_DIR=C:/Users/siban/Dropbox/CSAIL/Projects/12_Legal_Outcome_Predictor/00_data/v2/02_runs/01_binary/00_BERT_TRANSF_v2_FIX_50par_20ep/
 
-INPUT_DIR=/data/rsg/nlp/sibanez/02_LegalOutcomePredictor/00_data/v2/01_preprocessed/01_50pars_256_tok/04_full_binary_resampled_1.0
-OUTPUT_DIR=/data/rsg/nlp/sibanez/02_LegalOutcomePredictor/00_data/v2/02_runs/01_binary/02_BERT_TRANSF_v2_FIX_50par_10ep_resampled_1.0
+INPUT_DIR=/data/rsg/nlp/sibanez/03_LegalPredictor_v1/00_data/02_preprocessed/01_binary/01_selected_ECHR_arts/00_filtered
+OUTPUT_DIR=/data/rsg/nlp/sibanez/03_LegalPredictor_v1/00_data/03_runs/01_binary/01_selected_ECHR_arts/00_BERT_TRANSF_v2_FIX_50par_10ep/
 
 python train_v1.py \
     --input_dir=$INPUT_DIR \
     --output_dir=$OUTPUT_DIR \
-    --n_epochs=20 \
-    --batch_size=40 \
+    --n_epochs=10 \
+    --batch_size=60 \
     --shuffle_train=True \
     --train_toy_data=False \
     --len_train_toy_data=800 \
@@ -32,6 +32,7 @@ python train_v1.py \
 
 #read -p 'EOF'
 
-#--batch_size=50
+#--batch_size=40
+#--batch_size=25 / 0,1,2,3
 #--n_epochs=100
 #--max_n_pars=200
